4770b8c26799870b719f65551d96e1ca2270183f
hhadleydalt <159504608+hhadleydalt@users.noreply.github.com>
2024-02-11 02:08:23 -0500

    Update content.js

--- content.js
+++ content.js
@@ -14,17 +14,21 @@
 
         const promptExists = document.getElementsByClassName("prompt")[0]
 
-        if (!promptExists) {
+        if (!promptExists) { 
+
+            const title = document.querySelector("#title.style-scope.ytd-watch-metadata")
+            const titleColor = window.getComputedStyle(title).getPropertyValue('color');
+
             const container = document.createElement("div")
             container.className = "style-scope ytd-watch-metadata prompt"
-            container.style.backgroundColor = "white"
+            container.style.backgroundColor = "transparent"
             container.style.borderRadius = "1.75rem"
             container.style.padding = "0.5rem"
             container.style.marginTop = "1rem"
             container.style.marginBottom = "1rem"
-            container.style.border = "1px solid rgba(100, 100, 100)"
-            container.style.filter = "drop-shadow(0rem 0.5rem 0rem rgba(100, 100, 100))"
-            container.style.color = "rgba(100, 100, 100)"
+            container.style.border = "1px solid " + titleColor
+            container.style.borderBottom = "0.45rem solid " + titleColor
+            container.style.color = titleColor
             container.style.display = "flex"
             container.style.flexDirection = "row"
             container.style.alignItems = "center"
@@ -38,7 +42,7 @@
             logoBg.style.height = "2.5rem"
             logoBg.style.borderRadius = "2.5rem"
             logoBg.style.marginLeft = "0.5rem"
-            logoBg.style.backgroundColor = "rgba(100, 100, 100)"
+            logoBg.style.backgroundColor = titleColor
             logoBg.style.display = "flex"
             logoBg.style.alignItems = "center"
             logoBg.style.justifyContent = "center"
@@ -50,7 +54,6 @@
             logoIcon.style.width = "1.35rem"
             logoBg.appendChild(logoIcon)
 
-            const title = document.querySelector("#title.style-scope.ytd-watch-metadata")
             title.appendChild(container)
         }
     }
